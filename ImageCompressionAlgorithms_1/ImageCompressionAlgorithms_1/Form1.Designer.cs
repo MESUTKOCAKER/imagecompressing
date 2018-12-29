6d6be74a8969907bc43113e4a72a81a117786a41
MESUT KOÇAKER <mesut0723@gmail.com>
2018-12-29 22:43:18 +0300

    Add files via upload

---
+++ ImageCompressionAlgorithms_1/ImageCompressionAlgorithms_1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ImageCompressionAlgorithms_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(832, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(336, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Convert To Gray";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(698, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "View Original";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(549, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Compressing";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Image";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(130, 25);
+            this.label3.MaximumSize = new System.Drawing.Size(200, 18);
+            this.label3.MinimumSize = new System.Drawing.Size(200, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 18);
+            this.label3.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 408);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(402, 75);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(430, 408);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Viner Hand ITC", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(405, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "compressed";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 10;
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 483);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Mesut Koçaker     mesut0723@gmail.com";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ ImageCompressionAlgorithms_1/ImageCompressionAlgorithms_1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCompressionAlgorithms_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width += 1;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Width = this.Width / 2 - 1;
+            label2.Left = pictureBox2.Left + 3;
+            label2.Top = pictureBox2.Top + 3;
+        }
+
+        private Image openImage()
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Jpg|*.jpg";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                label3.Text = (file.FileName.Split('\\')[file.FileName.Split('\\').Length - 1]);
+                return Image.FromFile(file.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Please, Image Selected");
+                return null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image image = openImage();
+            if (image != null)
+            {
+                pictureBox1.Image = image;
+                button4.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            compressing();
+        }
+
+        private void compressing()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            #region MyRegion
+            Bitmap bit = new Bitmap(pictureBox1.Image);
+            Bitmap compres = new Bitmap(bit.Width / 2, bit.Height / 2);
+            int xx = 0, yy = 0;
+            for (int i = 0; i < bit.Width - 1; i += 2)
+            {
+                yy = 0;
+                for (int j = 0; j < bit.Height - 1; j += 2)
+                {
+                    int color11 = (bit.GetPixel(i, j).R + bit.GetPixel(i, j).G + bit.GetPixel(i, j).B) / 3;
+
+                    int color12 = (bit.GetPixel(i, j + 1).R + bit.GetPixel(i, j + 1).G + bit.GetPixel(i, j + 1).B) / 3;
+
+                    int color21 = (bit.GetPixel(i + 1, j).R + bit.GetPixel(i + 1, j).G + bit.GetPixel(i + 1, j).B) / 3;
+
+                    int color22 = (bit.GetPixel(i + 1, j + 1).R + bit.GetPixel(i + 1, j + 1).G + bit.GetPixel(i + 1, j + 1).B) / 3;
+
+                    compres.SetPixel(xx, yy, Color.FromArgb(color11, color12, color21, color22));
+                    yy++;
+                }
+                xx++;
+            }
+            pictureBox2.Image = compres;
+            pictureBox2.Image.Save(Application.StartupPath + "/imgComp/as" + label3.Text);
+            #endregion
+
+            sw.Stop();
+
+            double frequ = Stopwatch.Frequency;
+            double nanoSec = (Math.Pow(1000, 3)) / frequ;
+            double stopNanoSec = sw.ElapsedTicks * nanoSec;
+
+            MessageBox.Show("Successful Saving");
+            MessageBox.Show(string.Format("MiliSecond : {0}\nNanoSecond : {1}", sw.ElapsedMilliseconds.ToString(), stopNanoSec.ToString()));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            viewOrginal();
+        }
+
+        private void viewOrginal()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            #region MyRegion
+            Bitmap bit = new Bitmap(pictureBox1.Image); // 100
+            Bitmap compres = new Bitmap(bit.Width * 2, bit.Height * 2); //200
+            int xx = 0, yy = 0;
+            for (int i = 0; i < compres.Width - 1; i += 2)
+            {
+                yy = 0;
+                for (int j = 0; j < compres.Height - 1; j += 2)
+                {
+                    int a, r, g, b;
+                    a = bit.GetPixel(xx, yy).A;
+                    r = bit.GetPixel(xx, yy).R;
+                    g = bit.GetPixel(xx, yy).G;
+                    b = bit.GetPixel(xx, yy).B;
+
+                    compres.SetPixel(i, j, Color.FromArgb(a, a, a));
+                    compres.SetPixel(i, j + 1, Color.FromArgb(r, r, r));
+                    compres.SetPixel(i + 1, j, Color.FromArgb(g, g, g));
+                    compres.SetPixel(i + 1, j + 1, Color.FromArgb(b, b, b));
+                    yy++;
+                }
+                xx++;
+            }
+            pictureBox2.Image = compres;
+            pictureBox2.Image.Save(Application.StartupPath + "/imgComp/ViewOrginal_" + label3.Text);
+            #endregion
+
+            sw.Stop();
+
+            double frequ = Stopwatch.Frequency;
+            double nanoSec = (Math.Pow(1000, 3)) / frequ;
+            double stopNanoSec = sw.ElapsedTicks * nanoSec;
+
+            MessageBox.Show("Successfully Viewed Original");
+            MessageBox.Show(string.Format("MiliSecond : {0}\nNanoSecond : {1}", sw.ElapsedMilliseconds.ToString(), stopNanoSec.ToString()));
+
+        }
+
+        private void imageSave(Image image)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "jpg|*.jpg";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                image.Save(save.FileName);
+                MessageBox.Show("Successfully Save Image");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                bit = new Bitmap(pictureBox1.Image);
+                xx1 = 0;
+                timer1.Start();
+            }
+        }
+
+        int xx1 = 0;
+        Bitmap bit;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (xx1 < bit.Width)
+                {
+                    timer1.Stop();
+                    for (int j = 0; j < bit.Height; j++)
+                    {
+                        int color = (bit.GetPixel(xx1, j).R + bit.GetPixel(xx1, j).G + bit.GetPixel(xx1, j).B) / 3;
+                        bit.SetPixel(xx1, j, Color.FromArgb(color, color, color));
+                    }
+                    pictureBox1.Image = bit;
+                    xx1++;
+
+                    timer1.Start();
+                }
+                else
+                {
+                    i = 151;
+                    pictureBox1.Image = bit;
+                    timer1.Stop();
+                    imageSave(pictureBox1.Image);
+                }
+            }
+        }
+    }
+}
